21358699bbddaecf8e2fcd1f1473f1c07b704e9a
Akash <akashy0524@gmail.com>
2023-08-10 23:45:34 +0530

    cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,38 +1,155 @@
 // import {useSelector} from 'react-redux'
-// import {Grid, Typography,Box} from '@mui/material';
+// import {Grid, Typography,Box,styled} from '@mui/material';
 // //Components
-// import CartItem from './CartItem';
 
+// import CartItem from './CartItem';
+// import TotalView from './CartItem';
+
+
+// const Container = styled(Grid)`
+//    padding : 30px 135px;
+
+// `
+
+// const Header = styled(Box)`
+//     padding : 15px 24px;
+// `
 // const Cart =() =>{
 
+//     const { cartItems } = useSelector(state => state.cart);
 
-//             const {cartItems} = useSelector(state => state.cart);
+//     return (
+//         <>
+//             {
+//                 cartItems.length ?
+//                     <Container container>
 
+//                         <Grid item lg={9} md={9} sm={12} xs={12}>
+//                             <Header>
+//                                 <Typography>My Cart ({cartItems.length})</Typography>
+//                             </Header>
+//                             {   cartItems.map(item => (
+//                                 <CartItem item={item}/>
+//                             ))
+//                         }
+//                         </Grid>
 
-//             return(
-//                 <>
-                   
-//                        { cartItems.length ?
-//                             <Grid container >
-//                                 <Grid item lg={9} md={9} sm={12} xs={12}>
-//                                     <Box>
-//                                         <Typography>My Cart ({cartItems.length})</Typography>
-//                                     </Box>
-//                                     {
-//                                         cartItems.map(item =>(
-//                                             <CartItem />
-//                                         ))
-//                                     }
-//                                 <Grid item lg={3} md={3} sm={12} xs={12}>
+//                         <Grid item lg={3} md={3} sm={3} xs={12}>
+//                             <TotalView />
+//                         </Grid>
+//                     </Container>
 
-//                                 </Grid>
-//                             </Grid>
-//                         :   <div>Empty</div>
-//                     }
-//                 </>
-          
+//                 : <div>Empty</div>
+//             }
 
-//             )
-//     }
+//         </>
+
+        
+//     )
+// }
 
 // export default Cart;
+import { useEffect } from 'react';
+
+import { Box, Typography,  Grid, styled } from '@mui/material';
+import { useParams } from 'react-router-dom';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { addToCart, removeFromCart } from '../../redux/actions/cartActions';
+
+import TotalView from './TotalView';
+import EmptyCart from './EmptyCart';
+import CartItem from './CartItem';
+
+// import { post } from '../../utils/paytm';
+// import { payUsingPaytm } from '../../service/api';
+
+const Component = styled(Grid)(({ theme }) => ({
+    padding: '30px 135px',
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+        padding: '15px 0'
+    }
+}));
+
+const LeftComponent = styled(Grid)(({ theme }) => ({
+    paddingRight: 15,
+    [theme.breakpoints.down('sm')]: {
+        marginBottom: 15
+    }
+}));
+
+const Header = styled(Box)`
+    padding: 15px 24px;
+    background: #fff;
+`;
+
+// const BottomWrapper = styled(Box)`
+//     padding: 16px 22px;
+//     background: #fff;
+//     box-shadow: 0 -2px 10px 0 rgb(0 0 0 / 10%);
+//     border-top: 1px solid #f0f0f0;
+// `;
+
+// const StyledButton = styled(Button)`
+//     display: flex;
+//     margin-left: auto;
+//     background: #fb641b;
+//     color: #fff;
+//     border-radius: 2px;
+//     width: 250px;
+//     height: 51px;
+// `;
+
+const Cart = () => {
+    const cartDetails = useSelector(state => state.cart);
+    const { cartItems } = cartDetails;
+    const { id } = useParams();
+
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        if(cartItems && id !== cartItems.id)   
+            dispatch(addToCart(id));
+    }, [dispatch, cartItems, id]);
+
+    const removeItemFromCart = (id) => {
+        dispatch(removeFromCart(id));
+    }
+
+    // const buyNow = async () => {
+    //     let response = await payUsingPaytm({ amount: 500, email: 'kunaltyagi@gmail.com'});
+    //     var information = {
+    //         action: 'https://securegw-stage.paytm.in/order/process',
+    //         params: response    
+    //     }
+    //     post(information);
+    // }
+
+    return (
+        <>
+        { cartItems.length ? 
+            <Component container>
+                <LeftComponent item lg={9} md={9} sm={12} xs={12}>
+                    <Header>
+                        <Typography style={{fontWeight: 600, fontSize: 18}}>My Cart ({cartItems?.length})</Typography>
+                    </Header>
+                        {   cartItems.map(item => (
+                                <CartItem item={item} removeItemFromCart={removeItemFromCart}/>
+                            ))
+                        }
+                    {/* <BottomWrapper>
+                        <StyledButton onClick={() => buyNow()} variant="contained">Place Order</StyledButton>
+                    </BottomWrapper> */}
+                </LeftComponent>
+                <Grid item lg={3} md={3} sm={12} xs={12}>
+                    <TotalView cartItems={cartItems} />
+                </Grid>
+            </Component> : <EmptyCart />
+        }
+        </>
+
+    )
+}
+
+export default Cart;
